bdbd0a353392518f9b58cd782434d9463c0dced1
Cody <chorst@uw.edu>
2019-06-12 16:49:29 -0700

    existing treatment effect

--- src/vivarium_experiment_dementia/components/treatment.py
+++ src/vivarium_experiment_dementia/components/treatment.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import scipy.stats
 
 
 class ExistingTreatmentAlgorithm:
@@ -62,11 +63,11 @@ class ExistingTreatmentEffect:
     configuration_defaults = {
         'dementia_model': {
             'existing_dementia_treatment': {
-                'initial_effect_mean': 1.0,
-                'initial_effect_sd': 1.0,
+                'initial_effect_mean': 0.78,  # in terms of cdr_sb for now
+                'initial_effect_sd': 0.19,
                 'initial_effect_duration': 365,  # days
-                'stable_effect_mean': 1.0,
-                'stable_effect_sd': 1.0
+                'stable_effect_mean': 1.43,
+                'stable_effect_sd': 0.46
             }
         }
     }
@@ -76,6 +77,49 @@ class ExistingTreatmentEffect:
         return 'existing_treatment_effect'
 
     def setup(self, builder):
-        pass
+        self.config = builder.configuration['dementia_model']
 
-    # TODO: register value modifier that is
+        self.clock = builder.time.clock()
+
+        self.population_view = builder.population.get_view(['treatment_start'])
+
+        self.randomness = {'initial': builder.randomness.get_stream('init_effect_randomness'),
+                           'stable': builder.randomness.get_stream('stable_effect_randomness')}
+
+        self.initial_individual_effects = pd.Series()
+        self.stable_individual_effect = pd.Series()
+        builder.population.initializes_simulants(self.on_initialize_simulants)
+
+        self.initial_duration = pd.Timedelta(days=self.config['existing_dementia_treatment']['initial_effect_duration'])
+
+        builder.value.register_value_modifier('cdr_rate', modifier=self.adjust_cdr_rate)
+
+    def on_initialize_simulants(self, pop_data):
+
+        initial_effect_size = self.get_effect_sizes(pop_data, 'initial')
+        self.initial_individual_effects = self.initial_individual_effects.append(initial_effect_size)
+
+        stable_effect_size = self.get_effect_sizes(pop_data, 'stable')
+        self.stable_individual_effect = self.stable_individual_effect.append(stable_effect_size)
+
+    def get_effect_sizes(self, pop_data, type: str):
+        mean = self.config['existing_dementia_treatment'][f'{type}_effect_mean']
+        sd = self.config['existing_dementia_treatment'][f'{type}_effect_sd']
+
+        draw = self.randomness[type].get_draw(pop_data.index)
+        effect_size = scipy.stats.norm(mean, mean).ppf(draw)
+        effect_size[effect_size < 0] = 0.0
+        return pd.Series(effect_size, index=pop_data.index)
+
+    def adjust_cdr_rate(self, index, exposure):
+        pop = self.population_view.get(index)
+
+        # cdr rate is completely determined by treatment start date.
+        within_initial_index = pop.loc[(self.clock() - pop['treatment_start'] <= self.initial_duration)].index
+        stable_index = pop.loc[(self.clock() - pop['treatment_start'] > self.initial_duration)].index
+
+        exposure.loc[within_initial_index] = self.initial_individual_effects.loc[within_initial_index]
+        exposure.loc[stable_index] = self.stable_individual_effect.loc[stable_index]
+
+        import pdb
+        pdb.set_trace()
